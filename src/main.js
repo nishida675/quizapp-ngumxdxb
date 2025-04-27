3e0f5a144ec2c8c06675711a69d2611d072ca8c9
nishida675 <hoge93141@gmail.com>
2025-04-28 00:14:12 +0900

    WIP: before adding TailwindCSS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { mount } from 'svelte'
 
-import App from './App.svelte'
+// @ts-ignore
+import App from './App.svelte';
+
 
 const app = mount(App, {
   target: document.getElementById('app'),
