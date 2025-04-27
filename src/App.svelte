9627034622eef632250111e0fb0242d48b7f7578
nishida675 <hoge93141@gmail.com>
2025-04-28 02:25:34 +0900

    20250428

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <main>
-    <div>メニューバー</div>
-    <div>問題</div>
-    <div>
-      <button>選択肢1</button>
-      <button>選択肢2</button>
-      <button>選択肢3</button>
+    <!-- メニューバー -->
+    <div class="bg-red-200 flex justify-around">
+        <div>スコア:0</div>
+        <div>連続正解数:0</div>
     </div>
-  </main>
+    <div class="bg-green-200">問題</div>
+    <div class="bg-blue-200">
+        <button>選択肢1</button>
+        <button>選択肢2</button>
+        <button>選択肢3</button>
+    </div>
+</main>
